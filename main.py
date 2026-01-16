59e8c4d123eaa1c5c777b1c111e422ac85e00063
ianivr <ianivrubinstein@gmail.com>
2026-01-16 00:27:44 +0000

    add token count

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
+
+if response.usage_metadata is None:
+    raise RuntimeError("No usage metadata found in the response.")
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
 
 def main():
