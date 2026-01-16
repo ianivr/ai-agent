c75d06d9822bbf9e1ba1748a38af8f94a33bc255
ianivr <ianivrubinstein@gmail.com>
2026-01-16 00:46:08 +0000

    add user input and multiple messages

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,15 +11,27 @@ if api_key is None:
     raise RuntimeError("GEMINI_API_KEY is not set in the environment variables.")
 
 client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model='gemini-2.5-flash', contents=messages
 )
 
 if response.usage_metadata is None:
     raise RuntimeError("No usage metadata found in the response.")
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print("User prompt: ", args.user_prompt)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
 print(response.text)
+
+
 
 def main():
     print("Hello from ai-agent!")
