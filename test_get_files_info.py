dc241b38bf03d731b87a39efe0d583d90ee8da1a
ianivr <ianivrubinstein@gmail.com>
2026-01-17 21:36:59 +0000

    add a way to get files info

---
+++ test_get_files_info.py
@@ -0,0 +1,16 @@
+from functions.get_files_info import get_files_info
+
+print("Result for current directory:")
+print(get_files_info("calculator", "."))
+print("\n")
+
+print("Result for 'pkg' directory:")
+print(get_files_info("calculator", "pkg"))
+print("\n")
+
+print("Result for '/bin' directory:")
+print(get_files_info("calculator", "/bin"))
+print("\n")
+
+print("Result for '../' directory:")
+print(get_files_info("calculator", "../"))
