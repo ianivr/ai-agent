0454bbac0b9365b096f8395a22356ab584bd8e4d
ianivr <ianivrubinstein@gmail.com>
2026-01-21 02:03:51 +0000

    fix indentation

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,27 +1,31 @@
 import os
 from google.genai import types
 
-def get_files_info(working_directory, directory="."):
-  try:
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
-    if not valid_target_dir:
-      return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(target_dir):
-      return f'Error: "{directory}" is not a directory'
-    
-    list_of_items = os.listdir(target_dir)
-    items_data = []
-    for item in list_of_items:
-      path = os.path.join(target_dir, item)
-      file_size = os.path.getsize(path)
-      is_dir = os.path.isdir(path)
-      items_data.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
-    return "\n".join(items_data)
-  except Exception as err:
-    return f"Error: {err}"
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        list_of_items = os.listdir(target_dir)
+        items_data = []
+        for item in list_of_items:
+            path = os.path.join(target_dir, item)
+            file_size = os.path.getsize(path)
+            is_dir = os.path.isdir(path)
+            items_data.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+        return "\n".join(items_data)
+    except Exception as err:
+        return f"Error: {err}"
+
 
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
